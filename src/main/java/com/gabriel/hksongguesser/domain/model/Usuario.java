bf05d46c2d12918d3b5b909396f47017e1442d2f
Gabriel Guaitanele <gabrieljmym13@gmail.com>
2023-02-09 18:27:50 -0300

    add classe Usuario e usuarioRepository

---
+++ src/main/java/com/gabriel/hksongguesser/domain/model/Usuario.java
@@ -0,0 +1,18 @@
+package com.gabriel.hksongguesser.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String senha;
+}
